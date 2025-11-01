9ca1293d0e9f8588400715dfc976a02d5eeb6e93
salman <salmanmasroor07@gmail.com>
2025-11-01 11:49:48 -0700

    Perform different Operations

--- variables_dataTypes.py
+++ variables_dataTypes.py
@@ -7,3 +7,16 @@ b = a - b
 a = a - b
 
 print(f"Swap: a={a} & b={b}")
+
+
+# Check if a variable is an integer, float, or string and perform different operations based on the type.
+x = "Python"
+
+if isinstance(x, int):
+    print(f"{x} is integer")
+elif isinstance(x, float):
+    print(f"{x} is float")
+elif isinstance(x, str):
+    print(f"{x} is String")
+else:
+    print("unknown")
