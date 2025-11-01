de2f21a6ec3a14f4745420f3c9266a10187a1c61
salman <salmanmasroor07@gmail.com>
2025-11-01 12:16:29 -0700

    Perform different Operations

--- variables_dataTypes.py
+++ variables_dataTypes.py
@@ -20,3 +20,34 @@ elif isinstance(x, str):
     print(f"{x} is String")
 else:
     print("unknown")
+
+# Convert a string containing a number to an integer and a float
+data = "70"
+integer = int(data)
+print(integer, type(integer))
+float_no = float(integer)
+print(float_no , type(float_no))
+#  Find the largest and smallest numbers in a list.
+no_list = [12,23,43,12,34,12]
+temp = 0
+for number in no_list:
+    if number > temp:
+        temp = number
+print(temp)
+
+# Count the number of occurrences of a value in a list.
+no_list = [12,23,43,12,34,12]
+length = len(no_list)
+check = []
+count = 0
+for i in range(length):
+    count = 0
+    if(no_list[i] in check):
+        continue
+    else:
+        check.append(no_list[i])
+    for j in range(length):
+        if(no_list[i] == no_list[j]):
+            count += 1
+    print(no_list[i], "= ",count)
+
