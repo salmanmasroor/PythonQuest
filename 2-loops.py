573a146379e8bf78558d8c9a53ed71286107d87c
salmanmasroor <salmanmasroor07@gmai.com>
2025-11-03 12:49:56 +0500

    add Functions

--- 2-loops.py
+++ 2-loops.py
@@ -1,25 +1,35 @@
 # loops
 
 # Print all prime numbers between 1 and n
-n = int(input("Enter the number: "))
-for i in range(1,n + 1):
-    count = 0
-    for j in range(1,i + 1): # replace n wiht i
-        if i % j == 0:
-            count += 1
-    
-    if (count <= 2):
-        print(i)
+def prime_no(no: int) -> list:
+    result = []
+    for i in range(1,n + 1):
+        count = 0
+        for j in range(1,i + 1): # replace n wiht i
+            if i % j == 0:
+                count += 1
+        
+        if (count <= 2):
+            result.append(i)
+        
+    return result
 
 #  Print the Fibonacci sequence up to n terms
-n = int(input("Enter the number: "))
-a = 0
-b = 1
-print(a)
-print(b)
-for i in range(n-2):
-    c  = a + b
-    a = b
-    b = c
-    print(c)
+def fibonacci(n: int) -> list:
+    a = 0
+    b = 1
+    result = [a,b]
+    for i in range(n-2):
+        c  = a + b
+        a = b
+        b = c
+        result.append(c)
+    return result
 
+if __name__ == "__main__":
+    
+    n = int(input("Enter the number for iterate: "))
+    print(prime_no(n))
+
+    n = int(input("Enter the number for iterate for Fibonacci sequence: "))
+    print(fibonacci(n))
